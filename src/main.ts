915a4884331781a99908874322adaa05e00bd14c
monu rajj <148453989+monurajj@users.noreply.github.com>
2024-11-18 15:45:11 +0530

    Update main.ts with cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Enable CORS
+  app.enableCors(); 
+
+  await app.listen(4000);
 }
 bootstrap();
+
